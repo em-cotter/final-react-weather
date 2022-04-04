75df3a950512bd84cf208d6e14680d309b309d3c
em-cotter <ecotter22@gmail.com>
2022-04-04 09:30:16 +0200

    added footer with git link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York"/>
+        <Weather defaultCity="New York" />
+      </div>
+      <div className="footer"> 
+        This project was coded by Emma Cotter and is {""}
+        <a href="https://github.com/em-cotter/final-react-weather">
+          open sourced on GitHub
+        </a>
       </div>
     </div>
   );
