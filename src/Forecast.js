e7a398265421f756e355fa7604aa43f7ca4b76c9
em-cotter <ecotter22@gmail.com>
2022-04-08 10:15:26 +0200

    Start integration forecast api

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,57 +1,44 @@
 import axios from "axios";
-import React from "react";
+import React, {useState} from "react";
+import "./Forecast.css"
 
 export default function Forecast(props){
-  
+  let [loaded, setLoaded]= useState(false)
+  let[forecast, setForecast]= useState(null)
+  let iconUrl=`http://openweathermap.org/img/wn/${forecast[0].weather[0].icon}@2x.png`
 function handelResponse(response){
-console.log(response.data)
+
+setForecast(response.data.daily)
+setLoaded(true)
 }
-console.log(props)
-  let apiKey = "30b4b8df96ab8adabf4f389d73097df8";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let url=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&units=metric`
+
   
-  axios.get(url).then(handelResponse)
+  if (loaded){
     return (
       <div className="Forecast">
         <div className="row">
-          <div className="col-4 forecastBoxes">
-            <div className="ForecastDay">Wednesday</div>
+          <div className="col forecastBoxes">
+            <div className="ForecastDay">{forecast[0].dt}</div>
             <div>
               <img
-                src="http://openweathermap.org/img/wn/02d@2x.png"
+                src= {iconUrl}
                 alt="Icon"
                 className="forecastIcon"
               />
             </div>{" "}
-            H:13° L:14°
-          </div>
-          <div className="col-4 forecastBoxes">
-            <div className="ForecastDay">Thursday</div>
-            <div>
-              {" "}
-              <img
-                src="http://openweathermap.org/img/wn/02d@2x.png"
-                alt="Icon"
-                className="forecastIcon"
-              />
-            </div>{" "}
-            H:13° L:14°
-          </div>
-          <div className="col-4 forecastBoxes">
-            <div className="ForecastDay">Friday</div>
-            <div>
-              {" "}
-              <img
-                src="http://openweathermap.org/img/wn/02d@2x.png"
-                alt="Icon"
-                className="forecastIcon"
-              />
-            </div>{" "}
-            H:13° L:14°
+            <span> H:{Math.round(forecast[0].temp.max)}°</span>{" "}
+            <span>L:{Math.round(forecast[0].temp.min)}°</span>
           </div>
         </div>
       </div>
-    );
+    );} else{
+let apiKey = "30b4b8df96ab8adabf4f389d73097df8";
+let latitude = props.coordinates.lat;
+let longitude= props.coordinates.lon
+let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&units=metric`;
+
+axios.get(url).then(handelResponse);
+return null
+    }
 }
+
