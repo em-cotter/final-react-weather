63d6b24bdd5d63de25499c52faedbfb7d902ec7a
em-cotter <ecotter22@gmail.com>
2022-04-10 12:14:48 +0200

    Finishing touches

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Dublin" />
       </div>
       <div className="footer"> 
         This project was coded by Emma Cotter and is {""}
