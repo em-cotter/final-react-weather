92fbb4dae0ab9eb7909ec0d31fea126139dc64bb
em-cotter <ecotter22@gmail.com>
2022-05-31 15:16:06 +0200

    background colour

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import Weather from './Weather'
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Weather defaultCity="Dublin" />
-      </div>
-      <div className="footer"> 
-        This project was coded by Emma Cotter and is {""}
-        <a href="https://github.com/em-cotter/final-react-weather" target="_blank" rel="noreferrer">
-          open sourced on GitHub
-        </a>
+      <div className="Inside">
+        <div className="container">
+          <Weather defaultCity="Dublin" />
+        </div>
+        <div className="footer">
+          This project was coded by Emma Cotter and is {""}
+          <a
+            href="https://github.com/em-cotter/final-react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </div>
       </div>
     </div>
   );
